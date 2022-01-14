7e856dcf65dcb697c82395a490f4d87040f50163
VisheshBansal <vishesh.bansal@gmail.com>
2022-01-14 23:55:06 +0530

    fix: linting

--- app.py
+++ app.py
@@ -6,14 +6,17 @@ from src.modules.dnsenumeration import get_records
 # creating a Flask app
 app = Flask(__name__)
 
-@app.route('/ping', methods = ['GET', 'POST'])
+
+@app.route("/ping", methods=["GET", "POST"])
 def ping():
-	return jsonify({'response': 'pong'})
+    return jsonify({"response": "pong"})
 
-@app.route('/v1/api/domain/records', methods = ['POST'])
+
+@app.route("/v1/api/domain/records", methods=["POST"])
 def domainrecords():
-    domain = request.args.get('domain')
-    return jsonify({'response':get_records(domain)})
+    domain = request.args.get("domain")
+    return jsonify({"response": get_records(domain)})
 
-if __name__ == '__main__':
-	app.run(debug = True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- src/modules/dnsenumeration.py
+++ src/modules/dnsenumeration.py
@@ -1,5 +1,6 @@
 import dns.resolver
-import re,requests
+import re, requests
+
 
 def add_to_dict(dict_obj, key, value):
     """
@@ -13,44 +14,52 @@ def add_to_dict(dict_obj, key, value):
         dict_obj[key] = [dict_obj[key], value]
 
     return dict_obj
-    
+
 
 def fetch_CNAMErecords(target):
     """Function to get all CNAME records for a domain (especially for Cloudflare)"""
 
-    headers = {'Pragma': 'no-cache', 'Origin': 'https://dnsdumpster.com',
-                'Accept-Encoding': 'gzip, deflate, br',
-                'Accept-Language': 'en-US,en;q=0.9,it;q=0.8',
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                'Cache-Control': 'no-cache',
-                'Referer': 'https://dnsdumpster.com/',
-                'Connection': 'keep-alive', 'DNT': '1', }
+    headers = {
+        "Pragma": "no-cache",
+        "Origin": "https://dnsdumpster.com",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9,it;q=0.8",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "Cache-Control": "no-cache",
+        "Referer": "https://dnsdumpster.com/",
+        "Connection": "keep-alive",
+        "DNT": "1",
+    }
 
-    get_csrf_res = requests.get('https://dnsdumpster.com', headers=headers)
+    get_csrf_res = requests.get("https://dnsdumpster.com", headers=headers)
 
     try:
-        csrf_token = get_csrf_res.headers['Set-Cookie']
+        csrf_token = get_csrf_res.headers["Set-Cookie"]
         csrf_token = csrf_token[10:]
         csrf_token = csrf_token.split(";")[0]
     except Exception as e:
         print("Retrieving CSRF Token for DNSDumpster failed")
         return
 
-    cookies = {'csrftoken': csrf_token, }
+    cookies = {
+        "csrftoken": csrf_token,
+    }
 
-    data = [('csrfmiddlewaretoken', csrf_token), ('targetip', target), ('user', 'free')]
+    data = [("csrfmiddlewaretoken", csrf_token), ("targetip", target), ("user", "free")]
 
-    res = requests.post('https://dnsdumpster.com/', headers=headers,
-                        cookies=cookies, data=data)
+    res = requests.post(
+        "https://dnsdumpster.com/", headers=headers, cookies=cookies, data=data
+    )
     try:
         scraped = res.text
-        subdomain_finder = re.compile('\">(.*\.' + target + ')<br>')
+        subdomain_finder = re.compile('">(.*\.' + target + ")<br>")
         links = subdomain_finder.findall(scraped)
-    
+
     finally:
         return links
+
 
 def get_records(domain):
     """
@@ -60,96 +69,93 @@ def get_records(domain):
 
     # List of all types of existing records for a domain.
     types = [
-        'A',
-        'NS',
-        'MD',
-        'MF',
-        'CNAME',
-        'SOA',
-        'MB',
-        'MG',
-        'MR',
-        'NULL',
-        'WKS',
-        'PTR',
-        'HINFO',
-        'MINFO',
-        'MX',
-        'TXT',
-        'RP',
-        'AFSDB',
-        'X25',
-        'ISDN',
-        'RT',
-        'NSAP',
-        'NSAP-PTR',
-        'SIG',
-        'KEY',
-        'PX',
-        'GPOS',
-        'AAAA',
-        'LOC',
-        'NXT',
-        'SRV',
-        'NAPTR',
-        'KX',
-        'CERT',
-        'A6',
-        'DNAME',
-        'OPT',
-        'APL',
-        'DS',
-        'SSHFP',
-        'IPSECKEY',
-        'RRSIG',
-        'NSEC',
-        'DNSKEY',
-        'DHCID',
-        'NSEC3',
-        'NSEC3PARAM',
-        'TLSA',
-        'HIP',
-        'CDS',
-        'CDNSKEY',
-        'CSYNC',
-        'SPF',
-        'UNSPEC',
-        'EUI48',
-        'EUI64',
-        'TKEY',
-        'TSIG',
-        'IXFR',
-        'AXFR',
-        'MAILB',
-        'MAILA',
-        'ANY',
-        'URI',
-        'CAA',
-        'TA',
-        'DLV',
+        "A",
+        "NS",
+        "MD",
+        "MF",
+        "CNAME",
+        "SOA",
+        "MB",
+        "MG",
+        "MR",
+        "NULL",
+        "WKS",
+        "PTR",
+        "HINFO",
+        "MINFO",
+        "MX",
+        "TXT",
+        "RP",
+        "AFSDB",
+        "X25",
+        "ISDN",
+        "RT",
+        "NSAP",
+        "NSAP-PTR",
+        "SIG",
+        "KEY",
+        "PX",
+        "GPOS",
+        "AAAA",
+        "LOC",
+        "NXT",
+        "SRV",
+        "NAPTR",
+        "KX",
+        "CERT",
+        "A6",
+        "DNAME",
+        "OPT",
+        "APL",
+        "DS",
+        "SSHFP",
+        "IPSECKEY",
+        "RRSIG",
+        "NSEC",
+        "DNSKEY",
+        "DHCID",
+        "NSEC3",
+        "NSEC3PARAM",
+        "TLSA",
+        "HIP",
+        "CDS",
+        "CDNSKEY",
+        "CSYNC",
+        "SPF",
+        "UNSPEC",
+        "EUI48",
+        "EUI64",
+        "TKEY",
+        "TSIG",
+        "IXFR",
+        "AXFR",
+        "MAILB",
+        "MAILA",
+        "ANY",
+        "URI",
+        "CAA",
+        "TA",
+        "DLV",
     ]
-    
+
     for Type in types:
         try:
             answers = dns.resolver.resolve(domain, Type)
             for rdata in answers:
                 # Adds data to the result.
-                result = add_to_dict(result, Type, rdata.to_text())       
-                    
-    
+                result = add_to_dict(result, Type, rdata.to_text())
+
         except Exception as e:
-            pass # or pass            
-    
+            pass  # or pass
+
     try:
         # Try to fetch CNAME records (if any)
         cname = fetch_CNAMErecords(domain)
         for records in cname:
-            result = add_to_dict(result, 'CNAME', records)
-    
+            result = add_to_dict(result, "CNAME", records)
+
     except Exception as e:
-        pass # or pass
+        pass  # or pass
 
     finally:
         return result
-
-
